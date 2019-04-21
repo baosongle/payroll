db7c888be432f6bddd92dabe028cb1e2ddaa2d4c
baosongle <songlebao@outlook.com>
2019-04-21 21:45:58 +0800

    在 PayrollDatabase 中增加了获取所有雇员的方法 getAllEmployees

--- src/main/java/com/github/songlebao/payroll/database/PayrollDatabase.java
+++ src/main/java/com/github/songlebao/payroll/database/PayrollDatabase.java
@@ -2,7 +2,10 @@ package com.github.songlebao.payroll.database;
 
 import com.github.songlebao.payroll.model.Employee;
 
+import java.util.List;
+
 public interface PayrollDatabase {
+    List<Employee> getAllEmployees();
     Employee getEmployee(int empId);
     void addEmployee(int empId, Employee employee);
     void clear();
--- src/main/java/com/github/songlebao/payroll/database/PayrollDatabaseImpl.java
+++ src/main/java/com/github/songlebao/payroll/database/PayrollDatabaseImpl.java
@@ -2,11 +2,20 @@ package com.github.songlebao.payroll.database;
 
 import com.github.songlebao.payroll.model.Employee;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class PayrollDatabaseImpl implements PayrollDatabase {
     private Map<Integer, Employee> employees = new ConcurrentHashMap<>();
+
+    private PayrollDatabaseImpl() { }
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        return new LinkedList<>(employees.values());
+    }
 
     @Override
     public Employee getEmployee(int empId) {
