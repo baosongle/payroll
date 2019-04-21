eb366c5e1ad2c0c018190650cf0cb5823e485659
songlebao <songlebao@outlook.com>
2019-04-21 16:41:51 +0800

    增加了新增钟点工功能的类，但是没完成具体代码

---
+++ src/main/java/com/github/songlebao/payroll/command/AddHourlyEmployeeTransaction.java
@@ -0,0 +1,7 @@
+package com.github.songlebao.payroll.command;
+
+// 有些雇员是钟点工，按照他们雇员记录中的每小时报酬字段对他们支付，每天提交工作时间卡，记录了日期和工作时长，如果每天工作超过 8 个小时，
+// 超过的部分按照 1.5 倍支付，每周五对他们支付
+public class AddHourlyEmployeeTransaction {
+    //TODO 完成这个类和单元测试
+}
